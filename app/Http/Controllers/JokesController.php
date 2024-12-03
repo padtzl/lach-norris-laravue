699dc6dae9578ba0f147c061bf675cf871236393
padietzel <padietzel@gmail.com>
2024-12-03 10:36:03 +0100

    feature/us-2: moved logic to service; add fallback if database is unavailable; add job to decouple save process;

--- app/Http/Controllers/JokesController.php
+++ app/Http/Controllers/JokesController.php
@@ -2,21 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Http;
+use App\Services\JokesService;
+use Illuminate\Http\JsonResponse;
 
 class JokesController extends Controller
 {
-    public function index()
-    {
-        try {
-            $response = Http::get('https://api.chucknorris.io/jokes/random');
-            if ($response->successful()) {
-                $joke = $response->json()['value'];
+    private JokesService $jokesService;
 
-                return response()->json(['joke' => $joke]);
-            }
-        } catch (\Exception $e) {
-            return response()->json(['No jokes available.']);
-        }
+    public function __construct(JokesService $jokesService)
+    {
+        $this->jokesService = $jokesService;
+    }
+
+    public function index(): JsonResponse
+    {
+        $jokeData = $this->jokesService->getRandomJoke();
+        return response()->json($jokeData);
     }
 }
---
+++ app/Services/JokesService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services;
+
+use App\Jobs\SaveJokeJob;
+use App\Models\Joke;
+use Illuminate\Support\Facades\Http;
+use Exception;
+
+class JokesService
+{
+    private const API_URL = 'https://api.chucknorris.io/jokes/random';
+
+    public function getRandomJoke(): array
+    {
+        try {
+            $response = Http::get(self::API_URL);
+
+            if ($response->successful()) {
+                $jokeData = $response->json();
+
+                SaveJokeJob::dispatch(
+                    $jokeData['id'],
+                    $jokeData['value']
+                );
+
+                return [
+                    'joke' => $jokeData['value'],
+                    'source' => 'API'
+                ];
+            }
+            throw new Exception('Unable to retrieve joke via API'); //meeh
+
+        } catch (Exception $e) {
+            // Exception Handling - return fallback from database or error
+            return $this->getFallbackJoke();
+        }
+
+    }
+
+    private function getFallbackJoke(): array
+    {
+        $randomJoke = Joke::inRandomOrder()->first();
+
+        if (!$randomJoke) {
+            return [
+                'joke' => 'No jokes available at the moment.',
+                'source' => null,
+            ];
+        }
+
+        return [
+            'joke' => $randomJoke->value,
+            'source' => 'Database',
+        ];
+    }
+}
