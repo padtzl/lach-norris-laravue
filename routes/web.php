9ff631e6c58149ec1958fb5eb9a9b8e59b19c20a
padietzel <padietzel@gmail.com>
2024-12-03 23:26:41 +0100

    feature/us-4: add api route

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
 
 Route::get('/joke', [JokesController::class, 'index']);
 Route::get('/search', [JokesController::class, 'search']);
+Route::get('/analyze', [JokesController::class, 'analyze']);
 
 Route::get('/api-settings', [ApiSettingController::class, 'index']);
 Route::post('/api-settings', [ApiSettingController::class, 'toggle']);
